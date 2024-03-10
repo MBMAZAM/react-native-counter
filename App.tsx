18a9b9f4e92a545faac789435b70e75d38d35c7a
Mauricio Maza <maza_1506@hotmail.com>
2024-03-10 18:27:43 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { PaperProvider } from "react-native-paper";
 import { CounterM3Screen } from "./src/presentation/screens/CounterM3Screen";
 
 import IonIcon from 'react-native-vector-icons/Ionicons'
+
 import { BoxObjectModelScreen } from "./src/presentation/screens/BoxObjectModelScreen";
 import { DimensionScreen } from "./src/presentation/screens/DimensionScreen";
 import { PositionScreen } from "./src/presentation/screens/PositionScreen";
